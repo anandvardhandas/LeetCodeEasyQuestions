215ada96b69e5f038bce99f89f239fa14583c063
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2021-05-23 16:33:43 +0530

    Create 21MergeTwoSortedLinkedList.cs

---
+++ 21MergeTwoSortedLinkedList.cs
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        
+       ListNode merged = new ListNode();
+        ListNode current = merged;
+        
+        while(l1 != null && l2 != null)
+        {
+            
+                 if(l1.val <= l2.val)
+                 {
+                     current.next = l1;
+                     l1 = l1.next;
+                 }
+                else
+                {
+                    current.next = l2;
+                    l2 = l2.next;
+                }
+                                current = current.next;
+
+        }
+        
+        if(l2 != null)
+            current.next = l2;
+               
+            
+             if(l1 != null)
+                current.next = l1;
+        
+        return merged.next;
+        
+    }
+}
